0f3ac7a8b6e9eabb0ee8a33a1fdf5778feecf894
ki82 <akerstrom.christian@gmail.com>
2014-09-19 10:44:43 +0200

    Updating timing in Slowtest and some gradle artifacts

--- src/test/java/superheroes/SlowTest.java
+++ src/test/java/superheroes/SlowTest.java
@@ -4,22 +4,22 @@ import org.junit.Test;
 public class SlowTest {
 
 	@Test public void test1() throws InterruptedException {
-		Thread.sleep(3000);
+		Thread.sleep(1000);
 	}
 
 	@Test public void test2() throws InterruptedException {
-		Thread.sleep(4000);
+		Thread.sleep(1000);
 	}
 
 	@Test public void test3() throws InterruptedException {
-		Thread.sleep(4000);
+		Thread.sleep(1000);
 	}
 
 	@Test public void test4() throws InterruptedException {
-		Thread.sleep(4000);
+		Thread.sleep(1000);
 	}
 
 	@Test public void test5() throws InterruptedException {
-		Thread.sleep(4000);
+		Thread.sleep(1000);
 	}
 }
